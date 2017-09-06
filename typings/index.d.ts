45f4eef0ee548fe6e46baa51e73f2776f89a5c56
Nam Dinh <thunderbird0304@gmail.com>
2017-09-06 23:36:33 +0700

    add source

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/assertion-error/index.d.ts" />
+/// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/pixi.js/index.d.ts" />
+/// <reference path="globals/sinon/index.d.ts" />
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/app.ts',
+  output: {
+    filename: 'dist/bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        enforce: 'post',
+        test: /\.tsx?$/,
+        include: path.resolve(__dirname, "node_modules/pixi.js"),
+        loader: 'ify'
+      }
+    ]
+  },
+  externals: [
+        // Don't bundle pixi.js, assume it'll be included in the HTML via a script
+        // tag, and made available in the global variable PIXI.
+        {"pixi.js": "PIXI"}
+    ]
+}
